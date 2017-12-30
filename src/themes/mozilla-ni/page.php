aedf129db3a17e441c37e36f56c86974bd9f5d73
dgaitan <jdavid.gaitan@gmail.com>
2017-12-30 12:30:30 -0600

    some styles for page

--- src/themes/mozilla-ni/page.php
+++ src/themes/mozilla-ni/page.php
@@ -14,25 +14,20 @@
 
 get_header(); ?>
 
-	<div id="primary" class="content-area">
-		<main id="main" class="site-main">
-
-			<?php
-			while ( have_posts() ) : the_post();
-
-				get_template_part( 'template-parts/content', 'page' );
-
-				// If comments are open or we have at least one comment, load up the comment template.
-				if ( comments_open() || get_comments_number() ) :
-					comments_template();
-				endif;
-
-			endwhile; // End of the loop.
-			?>
-
-		</main><!-- #main -->
-	</div><!-- #primary -->
+	<main class="main pt-5 pb-5">
+		<div class="container">
+			<div class="row">
+				<div class="col-12">
+					<?php while ( have_posts() ) : the_post(); ?>
+						<h1 class="title mb-5"><?php the_title(); ?></h1>
+						<div>
+							<?php the_content(); ?>
+						</div>
+					<?php endwhile; ?>
+				</div>
+			</div>
+		</div>
+	</main>
 
 <?php
-get_sidebar();
 get_footer();
