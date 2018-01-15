d59723c4b3b3ef0a209e66e1477e96b190d304b2
dgaitan <jdavid.gaitan@gmail.com>
2018-01-15 15:29:47 -0600

    clean cache

--- src/themes/mozilla-ni/functions.php
+++ src/themes/mozilla-ni/functions.php
@@ -172,3 +172,15 @@ if ( defined( 'JETPACK__VERSION' ) ) {
 	require get_template_directory() . '/inc/jetpack.php';
 }
 
+// Remove WP Version From Styles	
+add_filter( 'style_loader_src', 'sdt_remove_ver_css_js', 9999 );
+// Remove WP Version From Scripts
+add_filter( 'script_loader_src', 'sdt_remove_ver_css_js', 9999 );
+
+// Function to remove version numbers
+function sdt_remove_ver_css_js( $src ) {
+	if ( strpos( $src, 'ver=' ) )
+		$src = remove_query_arg( 'ver', $src );
+	return $src;
+}
+
