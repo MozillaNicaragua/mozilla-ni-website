7d2a584eee7f9dc5b016708bb24ddd68df64325e
dgaitan <jdavid.gaitan@gmail.com>
2018-02-06 16:28:11 -0600

    add disqus

--- src/themes/mozilla-ni/single.php
+++ src/themes/mozilla-ni/single.php
@@ -47,9 +47,33 @@ $related_posts = new WP_Query( $related_posts_args );
 							<div class="post-navigation">
 								<?php the_post_navigation(); ?>
 							</div>
+							<div>
+							</div>
 						</div>
 					</div>
 					<?php endwhile; ?>
+				</div>
+				<div class="col-12 mb-5">
+					<div id="disqus_thread"></div>
+					<script>
+
+					/**
+					*  RECOMMENDED CONFIGURATION VARIABLES: EDIT AND UNCOMMENT THE SECTION BELOW TO INSERT DYNAMIC VALUES FROM YOUR PLATFORM OR CMS.
+					*  LEARN WHY DEFINING THESE VARIABLES IS IMPORTANT: https://disqus.com/admin/universalcode/#configuration-variables*/
+					/*
+					var disqus_config = function () {
+					this.page.url = PAGE_URL;  // Replace PAGE_URL with your page's canonical URL variable
+					this.page.identifier = PAGE_IDENTIFIER; // Replace PAGE_IDENTIFIER with your page's unique identifier variable
+					};
+					*/
+					(function() { // DON'T EDIT BELOW THIS LINE
+					var d = document, s = d.createElement('script');
+					s.src = 'https://mozilla-nicaragua.disqus.com/embed.js';
+					s.setAttribute('data-timestamp', +new Date());
+					(d.head || d.body).appendChild(s);
+					})();
+					</script>
+					<noscript>Please enable JavaScript to view the <a href="https://disqus.com/?ref_noscript">comments powered by Disqus.</a></noscript>
 				</div>
 				<div class="col-12 mb-5">
 					<h4 class="title mb-4">Tambien te puede interesar</h4>
